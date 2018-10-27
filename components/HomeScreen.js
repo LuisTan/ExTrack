ea88241752830461da022c80462c61176396bb85
ArcRequiem <nvtesoro@gmail.com>
2018-10-27 18:58:13 +0800

    add View of Funds and history events

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 
 import { 
     Text, 
-    View
+    View,
+    FlatList
 } from 'react-native';
 
 import styles from './Style.js';
@@ -11,10 +12,50 @@ import AppNoLeftHeader from './AppNoLeftHeader.js';
 export default class HomeScreen extends Component<Props> {
     constructor(props){
       super(props)
-      this.state = { text: '' };
+      this.state = {
+          text: '' ,
+          peso: 0,
+          sentimo: 0,
+          historyToday: [
+                        {key: 'Fare'},
+                        {key: 'Food'},
+                        {key: 'Supplies'},
+                        {key: 'Food'},
+                        {key: 'Hobby'},
+                        {key: 'Dropped'},
+                        {key: 'Fare'},
+                        {key: 'Tithe'},
+                        ]
+      };
     }
-
-
+    
+    pesoString(whole,part){
+        sentimo = whole % 1;
+        peso = Math.floor(whole / 1);
+        sentimo = Math.floor(sentimo / 1) + part;
+        if(sentimo >= 100){
+            peso = peso + Math.floor(sentimo/100);
+            sentimo = sentimo % 100;
+        }
+        pesoStr = '₱' + peso + ".";
+        sentimoStr = sentimo + '¢'
+        if(sentimo > 0){
+            if(peso == 0){
+                return sentimoStr;
+            }
+            if(sentimo < 10){
+                pesoStr = pesoStr + "0" + sentimo;
+                return pesoStr;
+            }
+            else{
+                pesoStr = pesoStr + sentimo;
+            }
+        }
+        else{
+            pesoStr = pesoStr + "00";
+        }
+        return pesoStr;
+    }
 
     render() {
       const {navigate} = () => {
@@ -30,7 +71,18 @@ export default class HomeScreen extends Component<Props> {
           {/*Content*/}
           <View  style={[styles.background,{flex:9}]}>
             <View style={[styles.container, {flex:1}]}>
-              <Text style={styles.welcome}>Home Screen</Text>
+              <Text style={styles.welcome}>{this.pesoString(this.state.peso,this.state.sentimo)}</Text>
+            </View>
+            <View style={[styles.container, {flex:4}]}>
+                <Text style={styles.welcome}>List of Events Today</Text>
+                <FlatList
+                    data={this.state.historyToday}
+                    renderItem={({item}) => <Text style={{
+                        padding: 10,
+                        fontSize: 18,
+                        height: 44,
+                      }}>{item.key}</Text>}
+                />
             </View>
             {/*  */}
           </View>
