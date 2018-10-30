3635d26d9aeb078cf3b1e81d268aaa4a9317e175
Brian Sy <brianpesy@gmail.com>
2018-10-30 18:35:50 +0800

    Added Earn and Spend screen along with input validation

--- components/EarnScreen.js
+++ components/EarnScreen.js
@@ -1,27 +1,39 @@
 import React, {Component} from 'react';
 import { Button, Container, Content, Form, Item, Input, Label, Text as NBText  } from 'native-base';
+import {StyleSheet, TextInput, Alert} from 'react-native';
 import {
     View
 } from 'react-native';
 
 import styles from './Style.js'
 import AppHeader from './AppHeader.js';
+import DropdownMenu from 'react-native-dropdown-menu';
 
 
 export default class StatusScreen extends Component<Props> {
     constructor(props){
       super(props)
       this.state = {
-        text: '',
-        username: ''
+        category: '',
+        others: '',
+        earn: ''
       };
     }
-    
+
+    _InpValidation = () => {
+      if((this.state.category == "Choose Category" || this.state.others == '') || isNaN(this.state.earn)){
+        Alert.alert("Please enter all valid categories");
+      } else {
+        this.props.navigation.goBack();
+      }
+    }
+
     _confirm = () => {
       this.props.navigation.goBack();
     }
     render() {
-
+      var data = [["Choose Category", "Food & Drinks", "Bills", "Transportation", "Grocery", "Shopping/Entertainment", "Maintenance/Repair", "Health/Medication", " "]]
+      // console.log("test")
       return (
         <View style={{flex:1}}>
           {/*Header*/}
@@ -30,20 +42,45 @@ export default class StatusScreen extends Component<Props> {
           </View>
           {/*Content*/}
           <View  style={[styles.background,{flex:9}]}>
+          <View style={{flex: 1}}>
+            <View style={{height: 0}} />
+            <DropdownMenu
+              style={{flex: 1}}
+              bgColor={'white'}
+              // tintColor={'#666666'}
+              activityTintColor={'green'}
+              // arrowImg={}
+              // checkImage={}
+              optionTextStyle={{color: '#333333'}}
+              // titleStyle={{color: '#333333'}}
+              maxHeight={300}
+              handler={(selection, row) => this.setState({category: data[selection][row]})}
+              data={data}
+            >
+            </DropdownMenu>
+          </View>
+
             <Container>
               <Content padder >
                 <Form>
                   <Item floatingLabel>
-                    <Label>Username</Label>
-                    <Input onChangeText={(username) => this.setState({username})} />
+                    <Label>Enter Other Category</Label>
+                    <Input onChangeText={(others) => this.setState({others})} />
                   </Item>
-                  <Item floatingLabel last>
-                    <Label>Password</Label>
-                    <Input />
+                  <Item floatingLabel>
+                    <Label>Earnings (Number only)</Label>
+                    <Input onChangeText={(earn) => this.setState({earn})}/>
                   </Item>
                 </Form>
                 <Button onPress={ ()=>
-                  this._confirm()
+                  // this._confirm()
+                  // this.EmptyInp
+                  this._InpValidation()
+                  // if (this.state.category.trim() == ""){
+                  //   this.setState(() => ({ nameError: "First name required."}));
+                  // } else {
+                  //   this.setState(() => ({ nameError: null}));
+                  // }
                 }><NBText>Confirm</NBText></Button>
               </Content>
             </Container>
--- components/SpendScreen.js
+++ components/SpendScreen.js
@@ -1,20 +1,39 @@
 import React, {Component} from 'react';
-
+import { Button, Container, Content, Form, Item, Input, Label, Text as NBText  } from 'native-base';
+import {StyleSheet, TextInput, Alert} from 'react-native';
 import {
-    Text, 
     View
 } from 'react-native';
 
 import styles from './Style.js'
 import AppHeader from './AppHeader.js';
+import DropdownMenu from 'react-native-dropdown-menu';
 
 
 export default class StatusScreen extends Component<Props> {
     constructor(props){
       super(props)
-      this.state = {text: ''};
+      this.state = {
+        category: '',
+        others: '',
+        spend: ''
+      };
     }
+
+    _InpValidation = () => {
+      if((this.state.category == "Choose Category" || this.state.others == '') || isNaN(this.state.spend)){
+        Alert.alert("Please enter all valid categories");
+      } else {
+        this.props.navigation.goBack();
+      }
+    }
+
+    _confirm = () => {
+      this.props.navigation.goBack();
+    }
+
     render() {
+      var data = [["Choose Category", "Food & Drinks", "Bills", "Transportation", "Grocery", "Shopping/Entertainment", "Maintenance/Repair", "Health/Medication", " "]]
 
       return (
         <View style={{flex:1}}>
@@ -24,9 +43,48 @@ export default class StatusScreen extends Component<Props> {
           </View>
           {/*Content*/}
           <View  style={[styles.background,{flex:9}]}>
-            <View style={[styles.container, {flex:1}]}>
-              <Text style={styles.welcome}>Spend Screen</Text>
-            </View>
+          <View style={{flex: 1}}>
+            <View style={{height: 0}} />
+            <DropdownMenu
+              style={{flex: 1}}
+              bgColor={'white'}
+              // tintColor={'#666666'}
+              activityTintColor={'green'}
+              // arrowImg={}
+              // checkImage={}
+              optionTextStyle={{color: '#333333'}}
+              // titleStyle={{color: '#333333'}}
+              maxHeight={300}
+              handler={(selection, row) => this.setState({category: data[selection][row]})}
+              data={data}
+            >
+            </DropdownMenu>
+          </View>
+
+            <Container>
+              <Content padder >
+                <Form>
+                  <Item floatingLabel>
+                    <Label>Enter Other Category</Label>
+                    <Input onChangeText={(others) => this.setState({others})} />
+                  </Item>
+                  <Item floatingLabel>
+                    <Label>Spendings (Number only)</Label>
+                    <Input onChangeText={(spend) => this.setState({spend})}/>
+                  </Item>
+                </Form>
+                <Button onPress={ ()=>
+                  // this._confirm()
+                  // this.EmptyInp
+                  this._InpValidation()
+                  // if (this.state.category.trim() == ""){
+                  //   this.setState(() => ({ nameError: "First name required."}));
+                  // } else {
+                  //   this.setState(() => ({ nameError: null}));
+                  // }
+                }><NBText>Confirm</NBText></Button>
+              </Content>
+            </Container>
           </View>
         </View>
       );
