56c213a5bb3e4bd5bc40f636c5bd42c7002eb6c2
ArcRequiem <nvtesoro@gmail.com>
2018-11-15 22:53:04 +0800

    insert placeholders for storage + fixes

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -11,54 +11,132 @@ import {
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import {  addRecord } from './RecordsReducer.js';
+import { addRecord } from './RecordsReducer.js';
 import styles from './Style.js';
 import AppNoLeftHeader from './AppNoLeftHeader.js';
+import { store } from './store.js'
 import { Button } from 'react-native-paper';
 
 class HomeScreen extends Component<Props> {
     constructor(props){
-        super(props)
-        this.state = {
-            amount: 1230.75,
-            spent: 315.25,
-            spentToday: [
-                {key: 'Fare', amount: -60.00},
-                {key: 'Food', amount: -95.00},
-                {key: 'Supplies', amount: -120.00},
-                {key: 'Hobby', amount: -30.00},
-                {key: 'Lost', amount: -10.25},
-            ],
-            historyToday: [
-                {key:'8', reason: 'Fare', amount: -30.00},
-                {key:'7', reason: 'Lost', amount: -10.25},
-                {key:'6', reason: 'Hobby', amount: -30.00},
-                {key:'5', reason: 'Earned', amount: 30.00},
-                {key:'4', reason: 'Supplies', amount: -120.00},
-                {key:'3', reason: 'Food', amount: -95.00},
-                {key:'2', reason: 'Fare', amount: -30.00},
-                {key:'1', reason: 'Allowance', amount: 1000.00},
-            ] 
+        super(props);
 
+        if(store.data_records == null){
+            net = 714.75;
+            moneySpent = 315.25;
+            items = [
+                {
+                    details: "Go Home",
+                    category: "Transportation",
+                    cost: 30.00,
+                    inout: "Spend",
+                    time: new Date().toLocaleTimeString(),
+                    key:'7'
+                },
+                {
+                    details: "Stolen",
+                    category: "Lost",
+                    cost: 10.25,
+                    inout: "Spend",
+                    time: new Date().toLocaleTimeString(),
+                    key:'6'
+                },
+                {
+                    details: "Art Stuff",
+                    category: "Others",
+                    cost: 150.00,
+                    inout: "Spend",
+                    time: new Date().toLocaleTimeString(),
+                    key:'5'
+                },
+                {
+                    details: "Part Time Work",
+                    category: "Salary",
+                    cost: 30.00,
+                    inout: "Earn",
+                    time: new Date().toLocaleTimeString(),
+                    key:'4'
+                },
+                {
+                    details: "Lunch",
+                    category: "Food & Drinks",
+                    cost: 95.00,
+                    inout: "Spend",
+                    time: new Date().toLocaleTimeString(),
+                    key:'3'
+                },
+                {
+                    details: "Go To Work",
+                    category: "Transportation",
+                    cost: 30.00,
+                    inout: "Spend",
+                    time: new Date().toLocaleTimeString(),
+                    key:'2'
+                },
+                {
+                    details: "Some Money From Dad",
+                    category: "Allowance",
+                    cost: 1000.00,
+                    inout: "Earn",
+                    time: new Date().toLocaleTimeString(),
+                    key:'1'
+                },
+            ];
+        }
+        else if(store.data_records.length == 0){
+            net = 0.00;
+            moneySpent = 0.00;
+            items = [];
+        }
+        else{
+            net = store.data_records[0].net;
+            if(store.data_records.date == new Date()){
+                moneySpent = store.data_records[0].total_spent;
+                items = store.data_records[0].items;
+            }
+            else{
+                moneySpent = 0.00;
+                items = [];
+            }
+        }
+
+        spendCategories = ["Food & Drinks", "Bills", "Transportation", "Grocery", "Shopping/Entertainment", "Maintenance/Repair", "Health/Medication", "Lost", "Others"];
+        spendingRecord =[];
+
+        for(x = 0; x < spendCategories.length; x++){
+            spendingRecord.push({
+                key: '' + x,
+                category: spendCategories[x],
+                cost: 0,
+            })
+        }
+
+        for(x = 0; x < items.length; x++){
+            for(c = 0; c < spendingRecord.length; c++){
+                if(spendingRecord[c].category === items[x].category)
+                    spendingRecord[c].cost = spendingRecord[c].cost + items[x].cost;
+            }
+        }
+
+        this.state = {
+            amount: net,
+            spent: moneySpent,
+            spentToday: spendingRecord,
+            historyToday: items,
         };
     }
     
-    pesoString=(whole,part)=>{
-        sentimo = (whole*100)%100;
-        peso = whole;
+    pesoString=(money,inout)=>{
+        absValMoney = money;
+        if(money < 0)
+            absValMoney = -money;
+        sentimo = Math.floor(absValMoney * 100) % 100;
+        peso = Math.floor(absValMoney);
         pesoStr = '';
         sentimoStr = '';
-        if(whole < 0){
-            peso = -peso;
+        if(inout === "Spend" && money != 0){
             pesoStr = '-';
             sentimoStr = '-';
-            sentimo= sentimo * -1;
-        }
-        peso = Math.floor(peso / 1);
-        sentimo = Math.floor(sentimo / 1) + part;
-        if(sentimo >= 100){
-            peso = peso + Math.floor(sentimo/100);
-            sentimo = sentimo % 100;
         }
         pesoStr = pesoStr + '₱' + peso + ".";
         sentimoStr = sentimoStr + sentimo + '¢';
@@ -98,28 +176,29 @@ class HomeScreen extends Component<Props> {
 
                 {/*Content*/}
                 <ScrollView style={[styles.background,{flex:1}]}>
-                    <View style={[styles.background,{flexDirection:'row', flex:1}]}>
-                        <View style={[styles.container,{flex:1,marginRight:5,marginBottom:5}]}>
-                            <Text style={[styles.welcome,{marginBottom:0}]}>Current Money</Text>
-                            <Text style={[styles.moneyDisplay,this.state.amount < 0 ? styles.moneySpent:styles.moneyEarned,{marginTop:5}]}>{this.pesoString(this.state.amount,0)}</Text>
+                    <View style={[styles.background,{flexDirection:'column', flex:1}]}>
+                        <View style={[styles.homeContainer,{flex:1}]}>
+                            <Text style={[styles.welcome,{}]}>Current Money</Text>
+                            <Text style={[styles.moneyDisplay,
+                                this.state.amount < 0 ? styles.moneySpent:styles.moneyEarned,{marginTop:0}]}>{this.pesoString(this.state.amount,"Earn")}</Text>
                         </View>
-                        <View style={[styles.container,{flex:1,marginLeft:5,marginBottom:5}]}>
-                            <Text style={[styles.welcome,{marginBottom:0}]}>Spent Today</Text>
-                            <Text style={[styles.moneyDisplay,this.state.spent > 0 ? styles.moneySpent:styles.moneyEarned,{marginTop:5}]}>{this.pesoString(this.state.spent,0)}</Text>
+                        <View style={[styles.homeContainer,{flex:1}]}>
+                            <Text style={[styles.welcome,{}]}>Spent Today</Text>
+                            <Text style={[styles.moneyDisplay,this.state.spent > 0 ? styles.moneySpent:styles.moneyEarned,{marginTop:0}]}>{this.pesoString(this.state.spent,"Spend")}</Text>
                         </View>
                     </View>
-                    <View style={[styles.container, {flex:2,marginTop:5,marginBottom:5}]}>
+                    <View style={[styles.homeContainer, {flex:2}]}>
                         <Text style={styles.welcome}>Cumulative Spending Today</Text>
                         <FlatList
                             data={this.state.spentToday}
-                            renderItem={({item}) => <Text style={styles.listItems}>{item.key}: <Text style={item.amount < 0 ? styles.moneySpent:styles.moneyEarned}>{this.pesoString(item.amount,0)}</Text></Text>}
+                            renderItem={({item}) => <Text style={styles.listItems}>{item.category}: <Text style={item.cost > 0 ? styles.moneySpent:styles.moneyEarned}>{this.pesoString(item.cost,"Spend")}</Text></Text>}
                         />
                     </View>
-                    <View style={[styles.container, {flex:3,marginTop:5}]}>
+                    <View style={[styles.homeContainer, {flex:3}]}>
                         <Text style={styles.welcome}>Spending Today History</Text>
                         <FlatList
                             data={this.state.historyToday}
-                            renderItem={({item}) => <Text style={styles.listItems}>{item.reason}:{"\t"}<Text style={item.amount < 0 ? styles.moneySpent:styles.moneyEarned}>{this.pesoString(item.amount,0)}</Text></Text>}
+                            renderItem={({item}) => <Text style={styles.listItems}>{item.category}: <Text style={item.inout === "Spend" ? styles.moneySpent:styles.moneyEarned}>{this.pesoString(item.cost,item.inout)}</Text></Text>}
                         />
                     </View>
                     {/*  */}
