5e1968681cf4af2c4cc7281f759b7c74b24368ee
ArcRequiem <nvtesoro@gmail.com>
2018-12-12 06:22:12 +0800

    Splash Screen Android

--- android/app/src/main/java/com/extrack/MainApplication.java
+++ android/app/src/main/java/com/extrack/MainApplication.java
@@ -3,6 +3,7 @@ package com.extrack;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.horcrux.svg.SvgPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new SvgPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
